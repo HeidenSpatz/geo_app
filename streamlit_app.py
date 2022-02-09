8424096f89a6971a1473048557d7228c1097549b
Robert Uwe Litschel <uwelitschel@gmail.com>
2022-02-09 17:22:11 +0100

    added geopy

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,13 @@ import pandas as pd
 import numpy as np
 import geemap.foliumap as geemap
 
+#OpenStreetMap API for geocoding
+from geopy.geocoders import Nominatim 
+#To avoid time out in API
+from geopy.extra.rate_limiter import RateLimiter
+
+
+
 #------------------
 # config
 st.set_page_config(page_title='Geo App Demo',
@@ -13,5 +20,48 @@ st.set_page_config(page_title='Geo App Demo',
 
 st.title("Geo App Demo")
 
+#------------------
+# get input
+st.header("please enter address and parameters")
+# street, housenumber, city, zip code
+col1, col2, col3, col4 = st.columns([4, 1, 3, 2])
+
+with col1:
+    street = col1.text_input('Street')
+
+with col2:
+    housenr = col2.text_input('Nr.')
+
+with col3:
+    zip = st.text_input('Zip Code')
+    
+with col4:
+    city = st.text_input('City')
+
+st.write('address: ', street, " ", housenr, ", ", zip, " ", city)
+
+
+geolocator = Nominatim(user_agent="my_user_agent")
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=2)
+
+address = street + " " + housenr + ", " + city + " " + zip
+
 m = geemap.Map(center=[50.126, 8.707], zoom=18)
-m.to_streamlit(height=600)
+if len(address) > 5:
+    
+    location = geolocator.geocode(address)
+
+    lat = location.latitude
+    lon = location.longitude
+
+    st.write(location.address)
+    st.write(lat, lon)
+    #st.write(f"Lat, Lon: {lat}, {lon}")
+    #st.write(location.raw)
+    popup = f"lat, lon: {lat}, {lon}"
+    
+    m.add_basemap("ROADMAP")
+    m.add_marker(location=(lat, lon), popup=popup)
+    m.to_streamlit(height=600)
+else:
+    m.to_streamlit(height=600)
