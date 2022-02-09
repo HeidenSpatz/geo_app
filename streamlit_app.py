ce1177f77fe57d8a2a690b393ce151e742910166
Robert Uwe Litschel <uwelitschel@gmail.com>
2022-02-09 16:54:06 +0100

    geemap

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import geemap.foliumap as geemap
+
 
 st.title("Geo App Demo")
+
+m = geemap.Map(center=[50.126, 8.707], zoom=14)
+m.to_streamlit(height=600)
