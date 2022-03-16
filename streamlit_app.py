2c47993b2f14a5b9232a85dc1524254eb58fceb4
Robert Uwe Litschel <uwelitschel@gmail.com>
2022-03-16 10:22:08 +0100

    added sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import get
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -14,76 +15,93 @@ from geopy.extra.rate_limiter import RateLimiter
 #------------------
 # config
 st.set_page_config(page_title='Geo App Demo',
-                #page_icon=":shark:",
+                page_icon=":shark:",
                 layout='wide')
-                #menu_items: Get help, Report a Bug, About
 
 
-st.title("Geo App Demo")
+# sidebar
+#------------------
+app_type = st.sidebar.selectbox(
+    "This can be used to structure the app.",
+    ("Find Location", "ImmoScout Offers"), 
+    index=0
+)
+
+
+if app_type == "Find Location":
+    st.header("please enter address and parameters")
+    # street, housenumber, city, zip code
+    col1, col2, col3, col4 = st.columns([4, 1, 3, 2])
+
+    with col1:
+        street = col1.text_input('Street')
+
+    with col2:
+        housenr = col2.text_input('Nr.')
+
+    with col3:
+        zip = st.text_input('Zip Code')
+        
+    with col4:
+        city = st.text_input('City')
+
+    st.write('address: ', street, " ", housenr, ", ", zip, " ", city)
+
+
+    geolocator = Nominatim(user_agent="my_user_agent")
+    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=2)
+
+    address = street + " " + housenr + ", " + city + " " + zip
+
+
+    if len(address) > 5:
+        
+        location = geolocator.geocode(address)
+
+        lat = location.latitude
+        lon = location.longitude
+
+        st.write(location.address)
+        st.write(lat, lon)
+        popup = f"lat, lon: {lat}, {lon}"
+
+        m = geemap.Map(center=[lat, lon], zoom=15)
+        m.add_basemap("ROADMAP")
+        m.add_marker(location=(lat, lon), popup=popup)    
+        m.to_streamlit(height=600)
+    else:
+        m = geemap.Map(center=[50.12978175, 8.693144895303579], zoom=15)
+        m.to_streamlit(height=600)
+
 
 url = "https://raw.githubusercontent.com/HeidenSpatz/geo_app/master/lat_lon.csv"
-df = pd.read_csv(url, encoding='utf-8')
 
-
-districts_selected = st.multiselect("Select Districts", df['regio3'].unique(), 'Bornheim')
-districts_data = df[df.regio3.isin(districts_selected)]
-
-
-
-m = geemap.Map(locate_control=True, plugin_LatLngPopup=False)
-m = geemap.Map(center=[50.12978175, 8.693144895303579], zoom=5)
-m.add_points_from_xy(districts_data, "lon", "lat")
-
-m.to_streamlit()
+@ st.cache
+def get_csv(url):
+    df = pd.read_csv(url, encoding='utf-8')
+    return df
 
 
 
+if app_type == "ImmoScout Offers":
+    st.title("ImmoScout Offers")
 
+    df = get_csv(url)
 
-#------------------
-# get input
-st.header("please enter address and parameters")
-# street, housenumber, city, zip code
-col1, col2, col3, col4 = st.columns([4, 1, 3, 2])
-
-with col1:
-    street = col1.text_input('Street')
-
-with col2:
-    housenr = col2.text_input('Nr.')
-
-with col3:
-    zip = st.text_input('Zip Code')
+    #districts_single = st.selectbox("Select One District", df['regio3'].unique())
+    districts_selected = st.multiselect("Select Districts", df['regio3'].unique(), 'Bornheim')
     
-with col4:
-    city = st.text_input('City')
 
-st.write('address: ', street, " ", housenr, ", ", zip, " ", city)
+    districts_data = df[df.regio3.isin(districts_selected)]
 
+    m = geemap.Map(locate_control=True, plugin_LatLngPopup=False)
+    m = geemap.Map(center=[50.12978175, 8.693144895303579], zoom=5)
+    m.add_points_from_xy(districts_data, "lon", "lat")
 
-geolocator = Nominatim(user_agent="my_user_agent")
-geocode = RateLimiter(geolocator.geocode, min_delay_seconds=2)
-
-address = street + " " + housenr + ", " + city + " " + zip
+    m.to_streamlit()
 
 
-if len(address) > 5:
-    
-    location = geolocator.geocode(address)
 
-    lat = location.latitude
-    lon = location.longitude
 
-    st.write(location.address)
-    st.write(lat, lon)
-    #st.write(f"Lat, Lon: {lat}, {lon}")
-    #st.write(location.raw)
-    popup = f"lat, lon: {lat}, {lon}"
 
-    m = geemap.Map(center=[lat, lon], zoom=15)
-    m.add_basemap("ROADMAP")
-    m.add_marker(location=(lat, lon), popup=popup)    
-    m.to_streamlit(height=600)
-else:
-    m = geemap.Map(center=[50.12978175, 8.693144895303579], zoom=15)
-    m.to_streamlit(height=600)
+
